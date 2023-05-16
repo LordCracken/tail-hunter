cc525b88967cfd865d520bc707f16dbe793ee82f
LordCracken <vladislav070201@gmail.com>
2023-05-16 19:51:19 +0300

    replace dummy index with real app index

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,19 @@
 import { defineConfig } from 'vite';
+const path = require('path');
 
 export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    },
+  },
 });
